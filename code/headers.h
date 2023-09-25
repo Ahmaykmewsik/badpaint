3378259f6246b3b54c02c6b213c2132fd2c9e4ed
Ahmaykmewsik <marclaroussini@alum.calarts.edu>
2023-09-25 14:56:10 -0400

    port in mem areas, math and string libraries
    making things feel like how I'm used to. A new project is always so exciting!

---
+++ code/headers.h
@@ -0,0 +1,7 @@
+
+
+#include "vn_intrinsics.h"
+#include "vn_math.h"
+#include "vn_string.h"
+
+#include "../includes/raylib/src/raylib.h"
--- code/main.cpp
+++ code/main.cpp
@@ -1,91 +1,78 @@
-#include "../includes/raylib/src/raylib.h"
 
-#include <stdlib.h>         // Required for: calloc(), free()
+#include "headers.h"
 
-#define MAX_FILEPATH_RECORDED   4096
-#define MAX_FILEPATH_SIZE       2048
+#define MAX_FILEPATH_RECORDED 4096
+#define MAX_FILEPATH_SIZE 2048
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    V2 windowDim = V2{1000, 1000};
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - drop files");
+    InitWindow(windowDim.x, windowDim.y, "badpaint");
+
+    GameMemory gameMemory = {};
+
+    InitializeArena(&gameMemory.permanentArena, Megabytes(100));
+    InitializeArena(&gameMemory.temporaryArena, Megabytes(1000));
+
+    G_STRING_TEMP_MEM_ARENA = &gameMemory.temporaryArena;
 
     int filePathCounter = 0;
-    char *filePaths[MAX_FILEPATH_RECORDED] = { 0 }; // We will register a maximum of filepaths
+    String filePaths[MAX_FILEPATH_RECORDED] = {};
 
-    // Allocate space for the required file paths
-    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++)
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
     {
-        filePaths[i] = (char *)RL_CALLOC(MAX_FILEPATH_SIZE, 1);
-    }
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
         if (IsFileDropped())
         {
             FilePathList droppedFiles = LoadDroppedFiles();
 
-            for (int i = 0, offset = filePathCounter; i < droppedFiles.count; i++)
+            for (int i = 0, offset = filePathCounter;
+                 i < droppedFiles.count;
+                 i++)
             {
                 if (filePathCounter < (MAX_FILEPATH_RECORDED - 1))
                 {
-                    TextCopy(filePaths[offset + i], droppedFiles.paths[i]);
+                    filePaths[offset + i] = CreateString(droppedFiles.paths[i]);
                     filePathCounter++;
                 }
             }
 
-            UnloadDroppedFiles(droppedFiles);    // Unload filepaths from memory
+            UnloadDroppedFiles(droppedFiles); 
         }
+
         //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            if (filePathCounter == 0) DrawText("Drop your files to this window!", 100, 40, 20, DARKGRAY);
-            else
+        if (filePathCounter == 0)
+            DrawText("Drop your files to this window!", 100, 40, 20, DARKGRAY);
+        else
+        {
+            DrawText("Dropped files:", 100, 40, 20, DARKGRAY);
+
+            for (unsigned int i = 0; i < filePathCounter; i++)
             {
-                DrawText("Dropped files:", 100, 40, 20, DARKGRAY);
+                if (i % 2 == 0)
+                    DrawRectangle(0, 85 + 40 * i, windowDim.x, 40, Fade(LIGHTGRAY, 0.5f));
+                else
+                    DrawRectangle(0, 85 + 40 * i, windowDim.y, 40, Fade(LIGHTGRAY, 0.3f));
 
-                for (unsigned int i = 0; i < filePathCounter; i++)
-                {
-                    if (i%2 == 0) DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.5f));
-                    else DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.3f));
-
-                    DrawText(filePaths[i], 120, 100 + 40*i, 10, GRAY);
-                }
-
-                DrawText("Drop new files...", 100, 110 + 40*filePathCounter, 20, DARKGRAY);
+                DrawText(filePaths[i].chars, 120, 100 + 40 * i, 10, GRAY);
             }
 
+            DrawText("Drop new files...", 100, 110 + 40 * filePathCounter, 20, DARKGRAY);
+        }
+
         EndDrawing();
-        //----------------------------------------------------------------------------------
+
+        ResetMemoryArena(&gameMemory.temporaryArena);
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++)
-    {
-        RL_FREE(filePaths[i]); // Free allocated memory for all filepaths
-    }
-
-    RayCloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    RayCloseWindow(); 
 
     return 0;
 }
