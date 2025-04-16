9505aabe3bcad8e32a2e07db65fce6edb1573a07
Ahmaykmewsik <ahmaykmewsik@gmail.com>
2025-04-16 13:41:58 +0200

    v0.0.3

--- code/main.h
+++ code/main.h
@@ -5,7 +5,7 @@
 
 void RunApp(PlatformWorkQueue *platformWorkQueue, GameMemory gameMemory, unsigned int threadCount);
 
-#define VERSION_NUMBER "v0.0.2"
+#define VERSION_NUMBER "v0.0.3"
 
 static String G_NOTIFICATION_MESSAGE = {};
 static float G_NOTIFICATION_ALPHA = 0.0f;
